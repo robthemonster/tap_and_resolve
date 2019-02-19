7d884965a76d34047e94e459ec21bb29c373f4ce
robthemonster <robthemonsterlevin@gmail.com>
2019-02-19 03:09:22 -0500

    .id tho

--- swipe_page.js
+++ swipe_page.js
@@ -4,7 +4,7 @@ function shuffleCard() {
     console.log(window.netlifyIdentity.currentUser());
     $.ajax({
         url: API_URL + "/randomCard",
-        data:{'uid':window.netlifyIdentity.currentUser()},
+        data:{'uid':window.netlifyIdentity.currentUser().id},
         type: 'GET'
     }).then(response => {
         console.log(response.card.imageUrl);
