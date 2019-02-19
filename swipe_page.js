0e84ddb4908577ef653f70b2a857781a34a945ad
robthemonster <robthemonsterlevin@gmail.com>
2019-02-19 03:02:59 -0500

    Testing identity

--- swipe_page.js
+++ swipe_page.js
@@ -1,10 +1,16 @@
-$.ajax( {
-    url:"https://api.tapandresolve.tk/randomCard",
-    type: 'GET'
-}).then(response => {
-    console.log(response.card.imageUrl);
-    $("#card_image").attr('src', response.card.imageUrl);
-    $("#card_name").text(response.card.name);
-    $("#info_text").text(response.card.text);
-    $("#flavor_text").text(response.card.flavor);
-});
+const API_URL = "https://api.tapandresolve.tk";
+shuffleCard();
+function shuffleCard() {
+    console.log(window.netlifyIdentity.currentUser());
+    $.ajax({
+        url: API_URL + "/randomCard",
+        data:{'uid':window.netlifyIdentity.currentUser()},
+        type: 'GET'
+    }).then(response => {
+        console.log(response.card.imageUrl);
+        $("#card_image").attr('src', response.card.imageUrl);
+        $("#card_name").text(response.card.name);
+        $("#info_text").text(response.card.text);
+        $("#flavor_text").text(response.card.flavor);
+    });
+}
