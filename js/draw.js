f8f6491cf25d06f6e4f36525e8c3fd9287cb4773
robthemonster <robthemonsterlevin@gmail.com>
2019-02-22 22:11:37 -0500

    css hell

--- js/draw.js
+++ js/draw.js
@@ -1,4 +1,5 @@
 let currentCard = -1;
+let currentFilters = {exclusive: false, flags: {B: true, U: true, G: true, R: true, W: true}};
 addNavBarAndLogin("liked.html", "blocked.html", "search.html", "#");
 
 function handleModalClose() {
@@ -16,6 +17,34 @@ function handleModalClose() {
 }
 
 shuffleCard();
+
+function handleFiltersChange() {
+    let exclusive = $("#color_filter_mode").prop('checked');
+    let [B_check, U_check, G_check, R_check, W_check] = [$("#B_check"), $("#U_check"), $("#G_check"), $("#R_check"), $("#W_check")];
+
+    currentFilters.flags['B'] = B_check.prop("checked");
+    currentFilters.flags['U'] = U_check.prop("checked");
+    currentFilters.flags['G'] = G_check.prop("checked");
+    currentFilters.flags['R'] = R_check.prop("checked");
+    currentFilters.flags['W'] = W_check.prop("checked");
+    currentFilters.exclusive = exclusive;
+}
+
+function highlightFilterMode() {
+    let checked = $("#color_filter_mode").prop('checked');
+    let [only, exactly] = [$("#only_span"), $("#exactly_span")];
+    let text_color = 'teal-text';
+    only.removeClass(checked ? text_color : "");
+    only.addClass(!checked ? text_color : "");
+    exactly.removeClass(checked ? "" : text_color);
+    exactly.addClass(checked ? text_color : '');
+
+}
+
+function openFilters() {
+    let [modal] = M.Modal.init($("#modal_filters"), {onCloseEnd: handleFiltersChange});
+    modal.open();
+}
 
 function likeCard() {
     $.post({
@@ -60,7 +89,7 @@ function shuffleCard() {
     }
     $.post({
         url: API_URL + "/randomCard",
-        data: {'userid': getUserId()},
+        data: {'userid': getUserId(), 'filter': JSON.stringify(currentFilters)},
     }).then(randomCard => {
         currentCard = randomCard.id;
         let imageurl = (randomCard.image_uris) ? randomCard.image_uris.border_crop : false;
